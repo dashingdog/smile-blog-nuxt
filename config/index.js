69f3356e59f561f54108506fc21db8a06ae7eaf4
v_zfahu <v_zfahu@tencent.com>
2020-07-10 21:25:28 +0800

    修改业务域名

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const Config = {
   // baseUrl: process.env.NODE_ENV === 'production' ? 'https://fitapp.dashingdog.cn' : 'http://127.0.0.1:3001',
-  baseUrl: 'http://api.dashingdog.cn',
-  // baseUrl: 'https://api.dashingdog.cn',
+  // baseUrl: 'http://api.dashingdog.cn',
+  baseUrl: 'https://api.dashingdog.cn',
   staticPath: 'resource.shirmy.me'
 }
 
